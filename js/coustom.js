3a146d7bcd6bf4c709de902038d52a0fbaf25542
JitShil123 <jitshil153@gmail.com>
2019-02-13 01:29:53 +0600

    my portfolio site

---
+++ js/coustom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$('.portfolio .ico .image').click(function(){
+		$('.portfolio .img2').hide(1000);
+	});
+	$('.portfolio .ico .laptop').click(function(){
+		$('.portfolio .img2').show(1000);
+	});
+})
