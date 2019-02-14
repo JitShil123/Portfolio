66d34467ba9724c7d9fecc02d7c3c115028b7faa
JitShil123 <jitshil153@gmail.com>
2019-02-14 21:50:11 +0600

    last one

--- js/coustom.js
+++ js/coustom.js
@@ -6,4 +6,22 @@ $(document).ready(function(){
 	$('.portfolio .ico .laptop').click(function(){
 		$('.portfolio .img2').show(1000);
 	});
-})
+
+});
+
+/*----------------type writer--------------------*/
+	$('#jit').typewriter({
+		text: ' BISHWAJIT SHIL'
+	});
+
+	$('#jit').typewriter({
+		waitingTime: 5000
+	});
+	$("#jit").typewriter({
+  		delay: 1000
+	});
+
+   new WOW().init();
+    
+
+
